9be84fe9225bf247400a48a829f25c7df5060c92
Anton Ra <9828281@gmail.com>
2024-01-31 15:21:57 +0300

    Initial Commit

---
+++ ShiftNotesApp/ShiftNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftNotesAppApp.swift
+//  ShiftNotesApp
+//
+//  Created by АнтохаПрограммист on 31.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShiftNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
